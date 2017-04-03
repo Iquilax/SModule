70c35c6f49957cf21d765ad9954a9160e292305e
Tuan Huynh <huynhminhanhtuan@gmail.com>
2017-04-03 23:20:09 +0700

    fix noty cont

--- SModule/Utils/SUtils.cs
+++ SModule/Utils/SUtils.cs
@@ -181,34 +181,34 @@ namespace SModule.Utils
 
             return result;
         }
-        public String getLinkChotot(ChototProduct product)
-        {
-            String resource = ChototUrlBuilder.resource;
-            int areaPostion = resource.IndexOf("\"" + product.areaId + "\"");
-            int targetAreaNamePos = -1;
-            foreach (var subStringFind in AllIndexesOf(resource,"name_url"))
-            {
-                if (subStringFind > areaPostion)
-                {
-                    targetAreaNamePos = subStringFind;
-                    break;
-                }
-            }
-            //String areaUrl = resource.Substring();
-            return null;
-        }
-        public static List<int> AllIndexesOf(this string str, string value)
-        {
-            if (String.IsNullOrEmpty(value))
-                throw new ArgumentException("the string to find may not be empty", "value");
-            List<int> indexes = new List<int>();
-            for (int index = 0; ; index += value.Length)
-            {
-                index = str.IndexOf(value, index);
-                if (index == -1)
-                    return indexes;
-                indexes.Add(index);
-            }
-        }
+        //public String getLinkChotot(ChototProduct product)
+        //{
+        //    String resource = ChototUrlBuilder.resource;
+        //    int areaPostion = resource.IndexOf("\"" + product.areaId + "\"");
+        //    int targetAreaNamePos = -1;
+        //    foreach (var subStringFind in AllIndexesOf(resource,"name_url"))
+        //    {
+        //        if (subStringFind > areaPostion)
+        //        {
+        //            targetAreaNamePos = subStringFind;
+        //            break;
+        //        }
+        //    }
+        //    //String areaUrl = resource.Substring();
+        //    return null;
+        //}
+        //public List<int> AllIndexesOf(this string str, string value)
+        //{
+        //    if (String.IsNullOrEmpty(value))
+        //        throw new ArgumentException("the string to find may not be empty", "value");
+        //    List<int> indexes = new List<int>();
+        //    for (int index = 0; ; index += value.Length)
+        //    {
+        //        index = str.IndexOf(value, index);
+        //        if (index == -1)
+        //            return indexes;
+        //        indexes.Add(index);
+        //    }
+        //}
     }
 }
