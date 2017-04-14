c8f0eae015be405c3d0097e4dfb1af290ac86d80
TuanHMA <tuanhma@outlook.com>
2017-04-14 23:41:17 +0700

    fx fb url

--- SModule/Utils/SUtils.cs
+++ SModule/Utils/SUtils.cs
@@ -113,7 +113,7 @@ namespace SModule.Utils
                         update.id = parseObject.id;
                         update.fullPicture = parseObject.fullPicture;
                         update.description = parseObject.description;
-                        update.url = String.Format("http://www.facebook.com/{0}/posts/{1}", update.id.Split('_').FirstOrDefault(), update.id.Split('_').LastOrDefault());
+                        update.url = parseObject.permalinkUrl;
                         if (trackedPlaced == "CT")
                         {
                             update.url = "https://www.chotot.com/toan-quoc/mua-ban?page=1&sp=0&suggested=1&q=" + trackProduct.title;
